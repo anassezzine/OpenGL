497504ad76272dcea71881932ede4aea83b2ac08
anass.ezzine <ezzin.anass@gmail.com>
2024-11-05 14:49:03 +0100

    house

--- Lighting_Multiple.cpp
+++ Lighting_Multiple.cpp
@@ -57,29 +57,34 @@ int main()
 	lightingShader.loadShaders("shaders/lighting_dir_point_spot.vert", "shaders/lighting_dir_point_spot.frag");
 
 	// Load meshes and textures
-	const int numModels = 2;
+	const int numModels = 3;
 	Mesh mesh[numModels];
 	Texture2D texture[numModels];
 
 	mesh[0].loadOBJ("models/floor.obj");
 	mesh[1].loadOBJ("models/road.obj");
+	mesh[2].loadOBJ("models/house.obj");
 
 
 	
 	texture[0].loadTexture("textures/tile_floor.jpg", true);
 	texture[1].loadTexture("textures/road.png", true);
+	texture[2].loadTexture("textures/house.png", true);
 
 	
 	// Model positions
 	glm::vec3 modelPos[] = {
 		glm::vec3(0.0f, 0.0f, 0.0f),	// floor
-		glm::vec3(0.0f,-2.5f,0.0f),		// road
+		glm::vec3(0.0f,-3.5f,0.0f),		// road
+		glm::vec3(0.0f,0.0f,-25.0f),		// house
+
 	};
 
 	// Model scale
 	glm::vec3 modelScale[] = {
 		glm::vec3(10.0f, 1.0f, 10.0f),	// floor
 		glm::vec3(0.5f, 0.5f, 0.5f),	// road
+		glm::vec3(1.0f, 1.0f, 1.0f),	// house
 	};
 
 	// Point Light positions
